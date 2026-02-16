e4f0e24d02556afe095fc4e168d84927f1398b72
Rickey Bowers Jr. <bitRAKE@gmail.com>
2026-02-15 21:41:41 -0700

    housekeeping

--- _clean.cmd
+++ _clean.cmd
@@ -2,10 +2,12 @@
 
 REM	Extra safe, only relative to this script:
 
-IF EXIST "%~dp0advanced_userdata" (
-    rmdir /S /Q "%~dp0advanced_userdata"
-)
-
 IF EXIST "%~dp0minimal.exe.WebView2" (
     rmdir /S /Q "%~dp0minimal.exe.WebView2"
+)
+IF EXIST "%~dp0game.exe.WebView2" (
+    rmdir /S /Q "%~dp0game.exe.WebView2"
+)
+IF EXIST "%~dp0advanced_userdata" (
+    rmdir /S /Q "%~dp0advanced_userdata"
 )
